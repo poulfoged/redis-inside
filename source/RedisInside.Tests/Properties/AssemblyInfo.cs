983297565045cf4cd339aaf3be4a96a9d7217344
Poul Foged Nielsen <pf@monzoom.net>
2015-09-16 22:07:58 +0200

    added readme, update to reds version

--- source/RedisInside.Tests/Properties/AssemblyInfo.cs
+++ source/RedisInside.Tests/Properties/AssemblyInfo.cs
@@ -1,33 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("s")]
+[assembly: AssemblyTitle("RedisInside.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ElasticsearchInside.Tests.Properties")]
+[assembly: AssemblyProduct("RedisInside.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2fbc98c1-6a6f-4925-a2cb-220bde180436")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.8.2101.0")]
+[assembly: AssemblyFileVersion("2.8.2101.0")]
